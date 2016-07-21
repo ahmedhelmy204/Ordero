742902e6758b3948988ea771d1fe24cf83ffb3c6
ahmedhelmy204 <ahmedhelmy204@hotmail.com>
2016-07-21 09:20:39 +0300

    User entity added.

---
+++ Ordero/Ordero.Core/Basis/BaseEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordero.Core.Basis
+{
+    /// <summary>
+    /// Base class for entities
+    /// </summary>
+    public abstract partial class BaseEntity
+    {
+        /// <summary>
+        /// Gets or sets entity id
+        /// </summary>
+        public int Id { get; set; }
+    }
+}
---
+++ Ordero/Ordero.Core/Domain/Users/User.cs
@@ -0,0 +1,47 @@
+﻿using Ordero.Core.Basis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordero.Core.Domain.Users
+{
+    /// <summary>
+    /// Represents a user
+    /// </summary>
+    public partial class User : AuditableEntity
+    {
+        /// <summary>
+        /// Gets or sets username
+        /// </summary>
+        public string Username { get; set; }
+
+        /// <summary>
+        /// Gets or sets email address
+        /// </summary>
+        public string Email { get; set; }
+
+        /// <summary>
+        /// Gets or sets password
+        /// </summary>
+        public string Password { get; set; }
+
+        /// <summary>
+        /// Gets or sets password salt
+        /// </summary>
+        public string PasswordSalt { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the user is active
+        /// </summary>
+        /// <remarks>true: if user is active, false: otherwise</remarks>
+        public bool IsActive { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the user has been deleted
+        /// </summary>
+        /// <remarks>true: if user has been deleted, false: otherwise</remarks>
+        public bool IsDeleted { get; set; }
+    }
+}
